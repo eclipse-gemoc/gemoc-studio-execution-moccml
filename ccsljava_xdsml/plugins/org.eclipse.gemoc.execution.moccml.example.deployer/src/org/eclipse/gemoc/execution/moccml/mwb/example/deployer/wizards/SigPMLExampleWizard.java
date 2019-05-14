ff8f531ddeb2464f86e7c938c3936ac536e47cc0
Didier Vojtisek <didier.vojtisek@inria.fr>
2019-05-14 11:49:35 +0200

    deployer for MoccmlSigPML example
    Signed-off-by: Didier Vojtisek <didier.vojtisek@inria.fr>

---
+++ ccsljava_xdsml/plugins/org.eclipse.gemoc.execution.moccml.example.deployer/src/org/eclipse/gemoc/execution/moccml/mwb/example/deployer/wizards/SigPMLExampleWizard.java
@@ -0,0 +1,26 @@
+package org.eclipse.gemoc.execution.moccml.mwb.example.deployer.wizards;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.gemoc.execution.moccml.example.deployer.Activator;
+import org.eclipse.gemoc.sequential.language.wb.sample.deployer.wizards.AbstractExampleWizard;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class SigPMLExampleWizard extends AbstractExampleWizard {
+
+	@Override
+	protected AbstractUIPlugin getContainerPlugin() {
+		return Activator.getDefault();
+	}
+
+	@Override
+	protected Collection<ProjectDescriptor> getProjectDescriptors() {
+		List<ProjectDescriptor> projects = new ArrayList<ProjectDescriptor>(1);
+		// extract project in a compilation friendly way, give the compiler a chance to be more efficient and succeed without a "clean all"
+		projects.add(new ProjectDescriptor(Activator.PLUGIN_ID, "zips/org.eclipse.gemoc.sample.ale.fsm.model_examples.zip", 	"org.eclipse.gemoc.sample.ale.fsm.model_examples"));
+return projects;
+	}
+
+}
