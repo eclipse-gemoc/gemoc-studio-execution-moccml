aea139f37e103b26c9ec88a111c62d291a797b8b
Erwan Bousse <erwan.bousse@ls2n.fr>
2019-10-10 17:04:46 +0200

    Re-remove getSolver and setSolver

--- ccsljava_xdsml/plugins/org.eclipse.gemoc.execution.concurrent.ccsljavaxdsml.api/src/org/eclipse/gemoc/execution/concurrent/ccsljavaxdsml/api/core/AbstractConcurrentExecutionEngine.java
+++ ccsljava_xdsml/plugins/org.eclipse.gemoc.execution.concurrent.ccsljavaxdsml.api/src/org/eclipse/gemoc/execution/concurrent/ccsljavaxdsml/api/core/AbstractConcurrentExecutionEngine.java
@@ -2,11 +2,12 @@ package org.eclipse.gemoc.execution.concurrent.ccsljavaxdsml.api.core;
 
 import java.util.List;
 
-import org.eclipse.gemoc.execution.concurrent.ccsljavaxdsml.api.moc.ISolver;
 import org.eclipse.gemoc.executionframework.engine.core.AbstractExecutionEngine;
 import org.eclipse.gemoc.trace.commons.model.trace.Step;
 
 public abstract class AbstractConcurrentExecutionEngine extends AbstractExecutionEngine {
+	
+	
 	public abstract AbstractConcurrentModelExecutionContext getConcurrentExecutionContext();
 
 	public abstract void recomputePossibleLogicalSteps();
@@ -18,9 +19,5 @@ public abstract class AbstractConcurrentExecutionEngine extends AbstractExecutio
 	public abstract ILogicalStepDecider getLogicalStepDecider();
 
 	public abstract void changeLogicalStepDecider(ILogicalStepDecider newDecider);
-
-	public abstract ISolver getSolver();
-
-	public abstract void setSolver(ISolver solver);
 
 }
