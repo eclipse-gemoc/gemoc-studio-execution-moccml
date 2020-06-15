d29f18af56bb92b4d02a96640ac015606d850a09
Gueguen Ronan <37030663+Kairides@users.noreply.github.com>
2020-06-15 16:01:31 +0200

    ValidationRule added to languageComponent in plugin (#45)
    * LanguageComponent added to plugin + new Kermeta3 validation rule
    * Validation on dsl removed from validation rules
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>

--- concurrent_addons/plugins/org.eclipse.gemoc.addon.metaprogramming/src/org/eclipse/gemoc/addon/metaprogramming/MOCCMLRule.java
+++ concurrent_addons/plugins/org.eclipse.gemoc.addon.metaprogramming/src/org/eclipse/gemoc/addon/metaprogramming/MOCCMLRule.java
@@ -1,71 +1,31 @@
 package org.eclipse.gemoc.addon.metaprogramming;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.gemoc.dsl.Dsl;
 import org.eclipse.gemoc.dsl.Entry;
-import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.IRule;
+import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.ILanguageComponentValidator;
 import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.Message;
 import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.Severity;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.ocl.cst.PackageDeclarationCS;
 import org.eclipse.ocl.xtext.basecs.PathElementWithURICS;
 import org.eclipse.ocl.xtext.completeoclcs.impl.PackageDeclarationCSImpl;
 import org.eclipse.ocl.xtext.completeoclcs.impl.ClassifierContextDeclCSImpl;
 
-public class MOCCMLRule implements IRule{
+public class MOCCMLRule implements ILanguageComponentValidator{
 
 	@Override
-	public Message execute(Dsl dsl) {
-		ArrayList<String> entriesNames = new ArrayList<String>();
-		
-		for (Entry e : dsl.getEntries()) {
-			entriesNames.add(e.getKey());
-		}
-		
-		if(!entriesNames.contains("k3")) {
-			return (new Message("Missing entry \"k3\"", Severity.ERROR));
-		}
-		
-		if(!entriesNames.contains("ecl")) {
-			return (new Message("Missing entry \"ecl\"", Severity.ERROR));
-		}
-		
-		return (new Message("", Severity.DEFAULT));
-	}
-
-	@Override
-	public Message execute(Entry entry) {
+	public Message validate(Entry entry) {
 		if("ecl".matches(entry.getKey())) {
 			URI uri = URI.createURI("platform:/resource/" + entry.getValue());
 			
 			if(!uri.isPlatformResource()) {
 				return (new Message("File for \"ale\" entry not in the workspace", Severity.ERROR));
 			}
-			
-			/*IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-			IFile file = root.getFile(new Path(entry.eResource().getURI().toPlatformString(true)));
-			
-			IProject proj = file.getProject();
-			IJavaProject jProj = JavaCore.create(proj);
-			
-			if(jProj == null) {
-				return (new Message("No project dsa in the workspace", Severity.ERROR));
-			}*/
-			
 			
 			ResourceSet rs = new ResourceSetImpl();
 			Resource res;
@@ -112,11 +72,6 @@ public class MOCCMLRule implements IRule{
 				 if(!hasContext) {
 					 return (new Message("No context in the ecl file.", Severity.WARNING));
 				 }
-				/*TODO: continu le taf ->
-				 * 	verifier l'import du fichier ecore,
-				 *  verifier la presence d'un package (surement instanceOf package)
-				 * 	verifier la presence de content (surement instanceOf content)
-				 */
 				
 			}catch(RuntimeException e) {
 				return (new Message("The file for the \"ecl\" does not exist", Severity.ERROR));
