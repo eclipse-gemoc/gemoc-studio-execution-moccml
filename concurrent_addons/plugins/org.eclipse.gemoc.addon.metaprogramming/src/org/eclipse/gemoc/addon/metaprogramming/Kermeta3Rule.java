d0846f1a5ef99f6e0dd6e4aa4e1e6f66e88c3e1e
Kairides <gueguen.ronan1@gmail.com>
2020-06-15 15:53:11 +0200

    Validation on dsl removed from validation rules
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>

--- concurrent_addons/plugins/org.eclipse.gemoc.addon.metaprogramming/src/org/eclipse/gemoc/addon/metaprogramming/Kermeta3Rule.java
+++ concurrent_addons/plugins/org.eclipse.gemoc.addon.metaprogramming/src/org/eclipse/gemoc/addon/metaprogramming/Kermeta3Rule.java
@@ -1,26 +1,19 @@
 package org.eclipse.gemoc.addon.metaprogramming;
 
-import org.eclipse.gemoc.dsl.Dsl;
 import org.eclipse.gemoc.dsl.Entry;
-import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.IRule;
+import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.ILanguageComponentValidator;
 import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.Message;
 import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.Severity;
 import org.eclipse.gemoc.xdsmlframework.extensions.kermeta3.Kermeta3RuleHelper;
 
-public class Kermeta3Rule implements IRule {
+public class Kermeta3Rule implements ILanguageComponentValidator {
 	
 	private Kermeta3RuleHelper k3Helper = new Kermeta3RuleHelper(false);
 
 	@Override
-	public Message execute(Dsl dsl) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Message execute(Entry entry) {
+	public Message validate(Entry entry) {
 		if("k3".matches(entry.getKey())) {
-			return (k3Helper.execute(entry));
+			return (k3Helper.execute(entry.getValue(), entry.eResource().getURI()));
 		}
 		return (new Message("", Severity.DEFAULT));
 	}
